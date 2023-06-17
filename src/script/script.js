e97d8b77d568448f6270de92800bd532cad84f5f
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-06-17 19:04:52 -0300

    Criado os método da classe

--- src/script/script.js
+++ src/script/script.js
@@ -1,26 +1,5 @@
-// Função que recebe um callback e um delay como parâmetro.
-function debounce(callback, delay) {
-  let timer; // Variável que armazena o timer.
+import Slide from "./slide.js"; // Importa a classe Slide.
 
-  // Ao executar a função debounce retorna e executa uma função anônima. Essa função anônima recebe os parâmetros passados na função debounce.
-  return (...args) => {
-    // Verifica se o timer existe se existir executa o if.
-    if (timer) {
-      clearTimeout(timer); // Limpa o timer.
-    }
-    timer = setTimeout(() => {
-      callback(...args); // Executa a função callback passando os parâmetros recebidos.
-      timer = null; // Atribui null a variável timer.
-    }, delay); // Atribui o timer a função setTimeout que recebe como parâmetro o callback e o delay.
-  };
-}
-
-// Função que será executada quando o evento de scroll for disparado.
-function onScroll() {
-  console.log("Opa eu fui executado!");
-}
-
-const debouncedScroll = debounce(onScroll, 200); // Chama a função debounce passando a função onScroll como valor do parâmetro callback e como valor do delay 1 segundo.
-
-// Adiciona o evento de scroll na janela.
-window.addEventListener("scroll", debouncedScroll);
+const slide = new Slide(".slide", ".slide-wrapper"); // Instancia a classe Slide.
+slide.init(); // Executa o método init.
+console.log(slide);
