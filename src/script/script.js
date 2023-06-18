4fb9b687777bf32449c6665719aa09b0d1b70459
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-06-18 14:26:42 -0300

    Configuracoes e index dos slides

--- src/script/script.js
+++ src/script/script.js
@@ -3,3 +3,4 @@ import Slide from "./slide.js"; // Importa a classe Slide.
 const slide = new Slide(".slide", ".slide-wrapper"); // Instancia a classe Slide.
 slide.init(); // Executa o método init.
 console.log(slide);
+slide.changeSlide(1);
